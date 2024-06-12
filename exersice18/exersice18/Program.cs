f4891c6e3b4f3c845b671b03472a1de801d1a072
Sepideh <sepideh.badran05@gmail.com>
2024-06-12 14:08:21 +0330

    exersice 18

---
+++ exersice18/exersice18/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("please enter the number :");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 ==0 && num % 5!= 0)
+{
+    Console.WriteLine("yes");
+}
